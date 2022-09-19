7a0a89a7e31ec59797e5c5f0b6efd81cb92cdbea
Farid Attamimi <tamimi2021farid@gmail.com>
2022-09-19 17:53:45 +0200

    Back-End2022

--- main.py
+++ main.py
@@ -33,20 +33,42 @@ def lon():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
-    pass
+    if request.method == 'GET':
+        print(request.args.get('error'))
+        return render_template('login.html', error=request.args.get('error'))
+    elif request.method == 'POST':
+        users = get_users()
+        attempted_password = hash_password(request.form['password'])
+        try:
+            username = request.form['username']
+            stored_password = users[username]
+        except KeyError:
+            return redirect(url_for('login', error=True))
+        
+        if stored_password == attempted_password:
+            session['username'] = request.form['username']
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('login', error=True))        
+    
 
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template('dashboared.html')
-    pass
-
+    if 'username' in session:
+        bonus_words = ['application', 'association', 'bark', 'anchor', 'authority', 'business', 'class', 'clap']
+        return render_template('dashboared.html', username=session['username'], words=bonus_words)
+    else:
+        return redirect(url_for('login'))
+    
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
-    # YOUR SOLUTION HERE
-    pass
+    try:
+        session.pop('username', None)
+    except:
+        pass
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
